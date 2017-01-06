689b953995f555c57faf1598336b8e8c3c87f710
xmascolara <xmascolara@users.noreply.github.com>
2017-01-06 14:02:06 +0100

    Update mkdir_dot_ssh.sh
    fixed a typo preventing the creation of the directories

--- mkdir_dot_ssh.sh
+++ mkdir_dot_ssh.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 # create .ssh-Directory for each user-directory in /home
+# set file owner and permission accordingly
 
-for i in $(ls /home/);
-do
-if [ ! -d /home/$i/.ssh/ ]; then
- mkdir /home/$i/.ssh/
- touch /home/$i/.ssh/authorized_keys
-#touch /root/$i
-fi;
-done
+for i in $(ls /home/); do
+if [ ! -d /home/$i/.ssh ]; then
+ cd /home/$i
+ mkdir .ssh
+ touch .ssh/authorized_keys
+ chown -R --reference=.bashrc .ssh
+ chmod -R 0600 .ssh
+ cd
+fi; done;
+
+#eof
